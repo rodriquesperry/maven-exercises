55c523f863fde43f8ae0ef596794e96dc11c69ed
Rodriques <rodriques.perry@gmail.com>
2021-11-12 11:51:47 -0600

    complete: Dependency Management Exercises through number 3

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter Something: ");
+        String userInput = scanner.nextLine();
+        if (!StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is not a number");
+            System.out.println(StringUtils.swapCase(userInput));
+            System.out.println(StringUtils.reverse(userInput));
+        }
+    }
+
+
+}
